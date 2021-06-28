e3c4d2d15194bbc3beb8496049c11e5979ce5672
Niket Nagwekar <niket1409@gmail.com>
2021-06-28 19:49:41 +0530

    master project setting up complete

---
+++ nnclothingshop/src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./homepage.styles.scss"
+
+import Directory from '../../components/directory/directory.components';
+
+function Homepage() {
+    return (
+        <div className="homePage">
+            <Directory />
+        </div>
+    )
+}
+
+export default Homepage
