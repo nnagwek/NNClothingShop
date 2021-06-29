7a760495455f914fa711e570cdbab82f7b8a1edf
Niket Nagwekar <niket1409@gmail.com>
2021-06-29 13:04:43 +0530

    update menu-items to navigate withRouter HOC

--- nnclothingshop/src/App.js
+++ nnclothingshop/src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import './App.css';
-import Homepage from './pages/homepage/homepage.component';
+import React from "react";
 
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+
+import "./App.css";
+import Homepage from "./pages/homepage/homepage.component";
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
 
 function App() {
   return (
-    <div className="App">
-      <Homepage />
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route path="/shop/hats" component={HatsPage} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
